7017c09a8e525cb0274b3de8e92c37a77fc6b58b
Przygoda <mprzygod1@PMINTL.NET>
2021-11-26 12:30:20 +0100

    tworzenie pliku

--- PasswordManager/Testy comita.py
+++ PasswordManager/Testy comita.py
@@ -1,1 +1,18 @@
-Comit test
+import os
+
+plik = open('password', 'w')
+userName = input("Please insert your Username: ")
+plik.write("Please insert your Username: " "|" + userName)
+
+password = input("Please insert your Password: ")
+plik.write("Please insert your Password: " "|" + password)
+
+def add():
+
+
+    # implement a method which gets two inputs from user: Username and Password,
+    # and then saves it in file named passwords.txt
+    # the data should be saved in new line, separated by pipe sign, like: <name>|<pass>
+    # the method should create the file if it doesn't exist
+    # the method doesn't return anything
+    pass
