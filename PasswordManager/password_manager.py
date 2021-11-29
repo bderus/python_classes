8b3b840e0518baac5de4d563db549bdf4a5da095
Kacki <pkacki@PMINTL.NET>
2021-11-29 19:19:06 +0100

    Zaimplementowano metode view w pliku

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -1,4 +1,4 @@
-import os
+import sys
 
 
 def add():
@@ -11,12 +11,19 @@ def add():
 
 
 def view():
-    # implement a method which reads the file passwords.txt,
-    # and then shows all Usernames and Password that are stored in it
-    # in a nice formatted way, like Username: <name> | Password: <password>
-    # the method should first check if the file exists
-    # the method doesn't return anything
-    pass
+
+    try:
+        check = open('password', 'r')
+    except OSError:
+        print('Error plik nie istnieije ')
+        sys.exit()
+
+    plik = open('password')
+    curses = 'red'
+    for linia in plik:
+        print(linia)
+
+
 
 
 while True:
@@ -31,3 +38,4 @@ while True:
     else:
         print("Invalid mode")
         continue
+        file = open("password", "a")
