e5c021869b1d5ab490a306d37030f09bdb0ffd14
Przygoda <mprzygod1@PMINTL.NET>
2021-12-02 12:12:57 +0100

    def gry

--- PasswordManager/wersja robocza.py
+++ PasswordManager/wersja robocza.py
@@ -1,6 +1,8 @@
 import app
 
-lista = [[1,2,3],[1,2,3],[1,2,3]]
+board = [[1,2,3],
+         [1,2,3],
+         [1,2,3]]
 player = input("Wybierz X lub O: ")
 if player == "X":
     print("Gracz pierwszy ma X, gracz drugi ma O ")
@@ -9,19 +11,22 @@ elif player == "O":
 else:
     print("Nieprawidłowy wybór ")
 
-tabelaGry = input("Wybierz 1, aby rozpocząć grę, wybierz 2, aby wrócić do wyboru menu: ")
+playerChoice = input("Wybierz 1, aby rozpocząć grę, wybierz 2, aby wrócić do wyboru menu: ")
 
-if tabelaGry == "1":
+def gameBoard():
+    for numeracjaPionowa, el in enumerate(board):
+        print(numeracjaPionowa, el)
+
+if playerChoice == "1":
     print("Rozpoczyna gracz pierwszy ")
-    print("   0  1  2")
-    numeracjaPionowa = 0
-    for el in lista:
-        print(numeracjaPionowa, lista[0])
-        numeracjaPionowa += 1
+    print("   a  b  c")
+    gameBoard()
+
+
 else:
     app.main_menu()
 
-playerOne = input("Wybierz pole(np 1:1, 1:2, 2:2 itd): ")
+
 
 
 
