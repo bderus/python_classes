d9797546bc30bd3abd1ee3ccc7e790925411df1a
Yoshimo94 <maciekprzygoda@interia.pl>
2021-12-06 21:11:08 +0100

    def gameScheme + def winCheck

--- PasswordManager/wersja robocza.py
+++ PasswordManager/wersja robocza.py
@@ -40,14 +40,66 @@ def gameBoard(player=0, row=0, column=0):
     board[row][column] = player
     for count, row in enumerate(board):
         print(count, row)
+def gameScheme():
 
-while True:
-    playerChoice = input("Wybierz X lub O: ")
-    playerChoicerow = input("Wybierz wiersz: ")
-    playerChoicecolumn = input("Wybierz kolumnę: ")
+    while True:
+        if winCheck() != True:
 
-    if gameBoard(playerChoice,int(playerChoicerow),int(playerChoicecolumn)):
-        print(board)
+            playerChoice = input("Wybierz X lub O: ")
+            if playerChoice == "X" or playerChoice == "0":
+
+                while True:
+                    playerChoicerow = input("Wybierz wiersz: ")
+                    if playerChoicerow == "0" or playerChoicerow == "1" or playerChoicerow == "2":
+
+                        while True:
+                            playerChoicecolumn = input("Wybierz kolumnę: ")
+                            if playerChoicecolumn == "0" or "1" or "2":
+                                gameBoard(str(playerChoice), int(playerChoicerow), int(playerChoicecolumn))
+                            break
+                    break
+
+
+def winCheck():
+        if board[0][0] == board[1][1] == board[2][2] == "X":
+            print("WIN")
+        elif board[2][0] == board[1][1] == board[0][2] == "X":
+            print("WIN")
+        elif board[0][0] == board[0][1] == board[0][2] == "X":
+            print("WIN")
+        elif board[1][0] == board[1][1] == board[1][2] == "X":
+            print("WIN")
+        elif board[2][0] == board[2][1] == board[2][2] == "X":
+            print("WIN")
+        elif board[0][0] == board[1][0] == board[2][0] == "X":
+            print("WIN")
+        elif board[0][1] == board[1][1] == board[2][1] == "X":
+            print("WIN")
+        elif board[0][2] == board[1][2] == board[2][2] == "X":
+            print("WIN")
+        elif board[0][0] == board[1][1] == board[2][2] == "0":
+            print("WIN")
+        elif board[2][0] == board[1][1] == board[0][2] == "0":
+            print("WIN")
+        elif board[0][0] == board[0][1] == board[0][2] == "0":
+            print("WIN")
+        elif board[1][0] == board[1][1] == board[1][2] == "0":
+            print("WIN")
+        elif board[2][0] == board[2][1] == board[2][2] == "0":
+            print("WIN")
+        elif board[0][0] == board[1][0] == board[2][0] == "0":
+            print("WIN")
+        elif board[0][1] == board[1][1] == board[2][1] == "0":
+            print("WIN")
+        elif board[0][2] == board[1][2] == board[2][2] == "0":
+            print("WIN")
+
+gameScheme()
+
+
+
+
+
     
 
 
