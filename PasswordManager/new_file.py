108d8bde29a28e80334e8da1c0433d7fb1487d74
Yoshimo94 <maciekprzygoda@interia.pl>
2021-12-08 21:04:19 +0100

    Wersja ostateczna

--- PasswordManager/new_file.py
+++ PasswordManager/new_file.py
@@ -27,28 +27,46 @@ def winCheck(playerChoice, board):
     elif board[0][2] == board[1][2] == board[2][2] == playerChoice:
         return True
 
+def drawCheck(board):
+    for row in board:
+        for element in row:
+            if element == "*":
+                return False
 
-def gameBoard(player, row=0, column=0):
-    if board[row][column] != "X" and board[row][column] != "0":
+    return True
+
+""" if board[0][0] != "*" and board[0][1] != "*" and board[0][2] != "*" and board[1][0] != "*" and \
+            board[1][1] != "*" and board[1][2] != "*" and board[2][0] != "*" and \
+            board[2][1] != "*" and board[2][2] != "*":
+        return True"""
+
+def gameBoard(playerChoice, playerChoicerow, playerChoicecolumn):
+    if board[playerChoicerow][playerChoicecolumn] != "X" and board[playerChoicerow][playerChoicecolumn] != "0":
         print("    0    1    2")
 
-        board[row][column] = player
-        if player == "X":
-            player = "0"
-        elif player == "0":
-            player = "X"
+        board[playerChoicerow][playerChoicecolumn] = playerChoice
+
 
         for count, row in enumerate(board):
             print(count, row)
 
-        if winCheck(str(player), board):
-            print("win")
+        if winCheck(str(playerChoice), board):
+            print("WIN")
             app.main_menu()
 
-        gameScheme(player)
+        if drawCheck(board):
+            print("DRAW")
+            app.main_menu()
+
+        if playerChoice == "X":
+            playerChoice = "0"
+        elif playerChoice == "0":
+            playerChoice = "X"
+
+        gameScheme(playerChoice)
     else:
         print("To pole jest już zajęte ")
-        pick_place(player)
+        pick_place(playerChoice)
 
 
 def gameScheme(playerChoice):
@@ -60,7 +78,7 @@ def pick_place(playerChoice):
     playerChoicecolumn = input("Wybierz kolumnę: ")
 
     if (playerChoicerow == "0" or playerChoicerow == "1" or playerChoicerow == "2") and (
-            playerChoicecolumn == "0" or "1" or "2"):
+            playerChoicecolumn == "0" or playerChoicecolumn == "1" or playerChoicecolumn == "2"):
         gameBoard(str(playerChoice), int(playerChoicerow), int(playerChoicecolumn))
     else:
         pick_place(playerChoice)
